64e7ec5818a0bcc7ee6f30d0ba3ff30767609788
tepochtli <tepochtli8@gmail.com>
2019-05-22 09:12:10 +0200

    Add state, type, and sound to Cortex kalliope_memory.
    Who add capatibility to retrieve actual played sound and sound list when send extra state (pause, play, ect..) or 'off'.
    
    And, when actual state is 'off', to not execute unecesary code if user send extra state (pause, play, etc...).

--- ambient_sound.py
+++ ambient_sound.py
@@ -9,13 +9,14 @@ import threading
 from time import sleep
 from kalliope.core.Utils import Utils
 from kalliope.core.NeuronModule import NeuronModule, InvalidParameterException
+from kalliope.core.Cortex import Cortex
 
 
 logging.basicConfig()
 logger = logging.getLogger("kalliope")
 
 pid_file_path = "pid.txt"
-fifo_file_path = "music-control"
+fifo_file_path = "music-control.pipe"
 sound_path = "player_content/"
 valid_ext = ['.mp3', '.ogg', '.wav', '.wma', '.amr', '.m3u']
 
@@ -27,32 +28,33 @@ class AmbientSound:
     def __str__(self):
         return "name: %s, file_extension: %s" % (self.name, self.file_extension)
 
-
 class SoundDatabase:
 
     def __init__(self, soundtype = "ambient"):
         self.available_sounds = self.load_available_sounds(soundtype)
 
     @staticmethod
-    def load_available_sounds(soundtype = "ambient"):
+    def load_available_sounds(soundtype):
         """
         Check all file in the sub folder "sounds" and return a list of AmbientSound object
         :return: list of AmbientSound object
         """
         list_ambient_sounds = list()
+        if SoundDatabase._is_valid_folder_type(soundtype):
 
-        list_sound_name_with_extension = \
-            [os.path.basename(x) for x in glob.glob(SoundDatabase.get_sound_folder_path(soundtype) + '/*')]
-        logger.debug("[Ambient_sounds] file in sound folder: %s" % list_sound_name_with_extension)
+            list_sound_name_with_extension = \
+                [os.path.basename(x) for x in glob.glob(SoundDatabase.get_sound_folder_path(soundtype) + '/*')]
+            logger.debug("[Ambient_sounds] file in sound folder: %s" % list_sound_name_with_extension)
 
-        for sound_with_extension in list_sound_name_with_extension:
-            tuple_name_extension = os.path.splitext(sound_with_extension)
-            # print tuple_name_extension
-            if tuple_name_extension[1] in valid_ext:
-                new_ambient_sound = AmbientSound(name=tuple_name_extension[0], file_extension=tuple_name_extension[1])
-                list_ambient_sounds.append(new_ambient_sound)
+            for sound_with_extension in list_sound_name_with_extension:
+                tuple_name_extension = os.path.splitext(sound_with_extension)
+                # print tuple_name_extension
+                if SoundDatabase._is_valid_extension(tuple_name_extension[1]):
+                    new_ambient_sound = AmbientSound(name=tuple_name_extension[0], file_extension=tuple_name_extension[1])
+                    list_ambient_sounds.append(new_ambient_sound)
 
         return list_ambient_sounds
+
 
     def get_sound_by_name(self, sound_name_to_find):
         """
@@ -72,6 +74,14 @@ class SoundDatabase:
         :return: AmbientSound object
         """
         return random.choice(self.available_sounds)
+
+    @staticmethod
+    def _is_valid_folder_type(state):
+        return (state in ["ambient", "music", "sound"])
+    
+    @staticmethod
+    def _is_valid_extension(state):
+        return (state in ['.mp3', '.ogg', '.wav', '.wma', '.amr', '.m3u'])
 
     @classmethod
     def get_neuron_path(cls):
@@ -106,7 +116,9 @@ class SoundDatabase:
 class Ambient_sound(NeuronModule):
     """
     Ambient sound neuron
-    Play a sound from the list of sound available in the sound folder
+    Play a sound from the list of sound available in a sound folder
+    Sounds folder type available are 'ambient', 'music' and 'sounds'
+    When sound started, can control mplayer with available commands. 
     """
     def __init__(self, **kwargs):
         super(Ambient_sound, self).__init__(**kwargs)
@@ -118,56 +130,85 @@ class Ambient_sound(NeuronModule):
         self.auto_stop_minutes = kwargs.get('auto_stop_minutes', None)
         
         self.is_playlist = False
-        self.extra_cmd = ["play","pause", "restart-song", "next-song", "back-song"]
+        
         # this is the target AmbientSound object if the user gave a sound_name to play.
         # this object will be loaded by the _is_parameters_ok function durring the check if the sound exist
         self.target_ambient_sound = None
 
-        # sound database
-        self.sdb = SoundDatabase(self.type)
-
         # message dict that will be passed to the neuron template
         self.message = dict()
-        # add the list of available sounds
-        self.message["available_sounds"] = list()
-        for sound in self.sdb.available_sounds:
-            self.message["available_sounds"].append(sound.name)
 
-        # check if sent parameters are in good state
-        if self._is_parameters_ok():
-            if self.state == "off":
-                self.stop_last_process()
-                self.clean_pid_file()
-            elif self.state in self.extra_cmd:
-                # To Do : 
-                #   - block next_song, back_song if no playlist or if no random
-                #     or if simple song select other creating other process
-                #   - add auto stop in paused mode for close unused process if user only pause and never stop
-                self._send_to_fifo_mplayer(self.state)
-            else:
-                # we stop the last process if exist
-                self.stop_last_process()
+        # last data saved in Cortex memory
+        # contain dict of state, type and sound_played
+        self._last_data = None
 
-                # if the user haven't given a sound name
-                if self.target_ambient_sound is None:
-                    # then we load one randomly
-                    self.target_ambient_sound = self.sdb.get_random_ambient_sound()
-                    logger.debug("[Ambient_sounds] Random ambient sound selected: %s" % self.target_ambient_sound)
+        # update last data
+        self._get_Cortex_ambient_data()
 
-                # then we can start a new process
-                if self.target_ambient_sound is not None:
-                    self.start_new_process(self.target_ambient_sound)
+        # if AmbientSound state is "off" and recive an extra_state, do nothink
+        # no extra state can be send if mplayer is off
+        # !!! what appens if mplayer started and last_data not available or modified by user !!!
+        if ((self._last_data.get('state') == "off" or \
+            self._last_data.get('state') is None) \
+            and self.state == "on") or \
+            (self._last_data.get('state') == "on") or \
+            self._is_normal_state(self.state):
 
-                    # give the current file name played to the neuron template
-                    self.message["playing_sound"] = self.target_ambient_sound.name
-                    self.message["is_playlist"] = self.is_playlist
-                    # run auto stop thread
-                    if self.auto_stop_minutes:
-                        thread_auto_stop = threading.Thread(target=self.wait_before_stop)
-                        thread_auto_stop.start()
+            # update self.type and self.sound_name when self.state in extra_states and mplayer is on
+            # to get SoundDatabase from corect folder
+            self.set_extra_state_params()
 
-            # give the message dict to the neuron template
-            self.say(self.message)
+            # sound database
+            self.sdb = SoundDatabase(self.type)
+
+            # add the list of available sounds
+            self.message["available_sounds"] = list()
+            for sound in self.sdb.available_sounds:
+                self.message["available_sounds"].append(sound.name)
+            
+            # check if sent parameters are in good state
+            if self._is_parameters_ok():
+                
+                if self.state == "off":
+                    self.stop_last_process()
+                    self.clean_pid_file()
+                    # update Cortex memory
+                    self._set_Cortex_ambient_data()
+                elif self._is_extra_state(self.state):
+                    # To Do : 
+                    #   - block next_song, back_song if no playlist or if no random
+                    #     or if simple song select other creating other process
+                    #   - add auto stop in paused mode for close unused process 
+                    #     if user only pause and never stop
+                    self.send_to_fifo_music_control(self.state)
+                else:
+                    # we stop the last process if exist
+                    self.stop_last_process()
+                    
+                    # if the user haven't given a sound name
+                    if self.target_ambient_sound is None:
+                        # then we load one randomly
+                        self.target_ambient_sound = self.sdb.get_random_ambient_sound()
+                        logger.debug("[Ambient_sounds] Random ambient sound selected: %s" % self.target_ambient_sound)
+
+                    # then we can start a new process
+                    if self.target_ambient_sound is not None:
+                        self.start_new_process(self.target_ambient_sound)
+
+                        # give the current file name played to the neuron template
+                        self.message["playing_sound"] = self.target_ambient_sound.name
+                        self.message["is_playlist"] = self.is_playlist
+                        # run auto stop thread
+                        if self.auto_stop_minutes:
+                            thread_auto_stop = threading.Thread(target=self.wait_before_stop)
+                            thread_auto_stop.start()
+                    # update Cortex memory
+                    self._set_Cortex_ambient_data()
+                
+                logger.error("[Ambient_sounds] Last data on Cortex: %s" % self._last_data)
+                # give the message dict to the neuron template
+                self.say(self.message)
+
 
     def wait_before_stop(self):
         logger.debug("[Ambient_sounds] Wait %s minutes before checking if the thread is alive" % self.auto_stop_minutes)
@@ -176,6 +217,8 @@ class Ambient_sound(NeuronModule):
         logger.debug("[Ambient_sounds] Time is over, Stop player")
         Utils.print_info("[Ambient_sounds] Time is over, stopping the ambient sound")
         self.stop_last_process()
+        # update Cortex memory
+        self._set_Cortex_ambient_data()
     
 
     def _is_parameters_ok(self):
@@ -184,8 +227,11 @@ class Ambient_sound(NeuronModule):
         :return: True if all given parameter are ok
         """
 
-        if self.state not in ["on", "off"] and self.state not in self.extra_cmd: 
+        if not self._is_normal_state(self.state) and not self._is_extra_state(self.state): 
             raise InvalidParameterException("[Ambient_sounds] State must be 'on', 'off', 'play', 'pause', 'restart-song', 'next-song', 'back-song'")
+
+        if not SoundDatabase._is_valid_folder_type(self.type) and self.state == "on":
+            raise InvalidParameterException("[Ambient_sounds] Sound Type %s, must be 'ambient', 'music' or 'sound'"% str(self.type))
 
         # check that the given sound name exist
         if self.sound_name is not None:
@@ -205,6 +251,85 @@ class Ambient_sound(NeuronModule):
             if self.auto_stop_minutes < 1:
                 raise InvalidParameterException("[Ambient_sounds] auto_stop_minutes must be set at least to 1 minute")
         return True
+    
+    def set_extra_state_params(self):
+        """
+        Set type and sound_name values from _last_data
+        If state is not "on" and if last_state is "off"
+
+        :return: True
+        """
+        
+        if isinstance(self._last_data, dict) and self._is_extra_state(self.state):
+            if self._last_data.get('type') is not None:
+                self.type = self._last_data.get('type')
+            
+            if self._last_data.get('sound') is not None and self.type is not None:
+                self.sound_name = self._last_data.get('sound')
+
+    def _get_Cortex_ambient_data(self):
+        """
+        Get ambient sount status from Cortex memory
+        Save data directly on self._last_data
+
+        :return: True
+        """
+        soundtype = Cortex.get_from_key("kalliope_ambient_sound_type")
+        if not SoundDatabase._is_valid_folder_type(soundtype):
+            soundtype = None
+
+        sound = Cortex.get_from_key("kalliope_ambient_sound_sound")
+        if soundtype is None:
+            sound = None
+
+        state = Cortex.get_from_key("kalliope_ambient_sound_state")
+        # if last state corrupted
+        if not self._is_normal_state(state) and state is not None:
+            state = None
+            # stop the last process if exist
+            self.stop_last_process()
+            # update Cortex memory
+            self._set_Cortex_ambient_data()
+        
+        self._last_data = {
+            "state": state,
+            "type": soundtype,
+            "sound": sound
+        }
+        return True
+
+    
+    def _set_Cortex_ambient_data(self):
+        """
+        Save ambient sount status on Cortex memory
+        And update self._last_data
+        """
+        if self.state == "on":
+            sound = None
+            if self.target_ambient_sound is not None:
+                sound = self.target_ambient_sound.name
+            self._last_data = {
+                "state": self.state,
+                "type": self.type,
+                "sound": sound
+                }
+            Cortex.save("kalliope_ambient_sound_state", self.state)
+            Cortex.save("kalliope_ambient_sound_type", self.type)
+            Cortex.save("kalliope_ambient_sound_sound", sound)
+        elif self.state == "off":
+            self._last_data = {"state": self.state, "type": None, "sound": None}
+
+            Cortex.save("kalliope_ambient_sound_state", self.state)
+            Cortex.save("kalliope_ambient_sound_type", None)
+            Cortex.save("kalliope_ambient_sound_sound", None)
+    
+    @staticmethod
+    def _is_normal_state(state):
+        return (state in ["on","off"])
+
+    @staticmethod
+    def _is_extra_state(state):
+        return (state in ["mute","unmute", "play","pause", "restart-song", "next-song", "back-song"])
 
     @staticmethod
     def _get_fifo_file_path():
@@ -233,12 +358,12 @@ class Ambient_sound(NeuronModule):
         return None
 
     @classmethod
-    def _send_to_fifo_mplayer(cls, cmd):
+    def send_to_fifo_music_control(cls, cmd):
         """
         Send a command to mplayer using fifo file
 
-        :param cmd: command key to translate and send to mplayer
-        :return: True or False depending if command sended to mplayer fifo
+        :param cmd: command key to translate
+        :return: Music control fifo path or None
         """
         # get pid and absolute path
         pid = cls.load_pid()
@@ -270,7 +395,7 @@ class Ambient_sound(NeuronModule):
                         file_open.close()
                     return True                   
             except IOError as e:
-                logger.error("[Ambient_sounds] I/O error on fifo file %s : %s", str(absolute_fifo_file_path),str(e))
+                logger.debug("[Ambient_sounds] I/O error on fifo file %s : %s", str(absolute_fifo_file_path),str(e))
                 return False      
             except Exception as e:
                 logger.error("[Ambient_sounds] Unable to send command to mplayer. Exception error: %s", str(e))
@@ -319,6 +444,8 @@ class Ambient_sound(NeuronModule):
                 return False
         return False
 
+    
+
     def stop_last_process(self):
         """
         stop the last mplayer process launched by this neuron
@@ -350,7 +477,7 @@ class Ambient_sound(NeuronModule):
         :return:
         """
         absolute_fifo_file_path = self._get_fifo_file_path()
-        if target_ambient_sound.file_extension in valid_ext:
+        if SoundDatabase._is_valid_extension(target_ambient_sound.file_extension):
             mplayer_exec_path = [self.mplayer_path]
             mplayer_options = ['-slave', '-quiet', '-loop', '0']
             if absolute_fifo_file_path is not None:
@@ -382,7 +509,7 @@ class Ambient_sound(NeuronModule):
 
             logger.debug("[Ambient_sounds] Mplayer started, pid: %s" % pid)
         else:
-            logger.error("[Ambient_sounds] Extension error : when attemping to play %s. Extension accepted are %s", str(target_ambient_sound.name + target_ambient_sound.file_extension), str(valid_ext))
+            logger.error("[Ambient_sounds] SoundDatabase corupted : when attemping to play %s. Extension type is not valid.", str(target_ambient_sound.name + target_ambient_sound.file_extension))
 
     @staticmethod
     def clean_pid_file():
